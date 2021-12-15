5ced82473758b93b82aa68ec5c3e926a63b9f951
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2021-12-15 17:12:06 +0100

    snack-3

---
+++ snack-3/js/main.js
@@ -0,0 +1,14 @@
+const printPow= document.getElementById("print");
+const response= document.getElementById("response");
+
+let exp=1;//esponente
+let max=1000;//massima potenza voluta
+let number = 2;//numero da elevare
+printPow.addEventListener("click", () =>{
+    response.classList.add("container", "bg-dark", "bg-gradient", "text-white", "p-4", "mt-1");
+    while (number**exp<1000){
+        response.innerHTML+=(`${number} elevato a ${exp} restituisce ${number**exp}<hr />`);
+        exp++;
+    }
+    
+})
