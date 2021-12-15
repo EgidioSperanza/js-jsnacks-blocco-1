a2f57332cafe3b49b1db93b8d33d84327b2da00a
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2021-12-15 16:51:46 +0100

    snack-2

---
+++ snack-2/js/main.js
@@ -0,0 +1,17 @@
+const sendNmax= document.getElementById("sendNmax");
+const response= document.getElementById("response");
+let userNmax;
+sendNmax.addEventListener("click", () =>{
+    response.innerHTML="";
+    let userN = prompt("Inserisci un numero composto al massimo da 3 cifre");
+    if(userN.length>3 || userN==="" ||isNaN(userN)){
+        alert("L'inserimento deve essere un numero di massimo 3 cifre e quindi nessun carattere e nessuno spazio")
+        return;
+    }else{
+        userNmax = userN;
+        response.classList.add("container", "bg-dark", "bg-gradient", "text-white", "p-4", "mt-1");
+        for(let i=1; i<=userN;i++){
+            response.innerHTML+=(`Il quadrato del numero ${i} è ${i**2} <hr />`);
+            }
+    }
+})
