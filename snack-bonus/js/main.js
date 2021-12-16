d647ed5031a78cac9ebb6275a2c81b8103f09420
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2021-12-16 08:58:17 +0100

    bonus aggiornato

--- snack-bonus/js/main.js
+++ snack-bonus/js/main.js
@@ -3,25 +3,30 @@ const response = document.getElementById("response");
 const num = document.getElementById("num");
 num.value="";
 let max;
-sendNumber.addEventListener("click", ()=>{
-  max = parseInt(num.value);
-  if(max>999 || isNaN(max)){
-    num.value="";
-    num.placeholder="3 Cifre, no caratteri";
-    return false;
-  }
-  let fibonacci=[0,1];
-  response.textContent="";
-  // console.log(fibonacci[0]);//DEBUG
-  // console.log(fibonacci[1]);//DEBUG
-  fibonacciSequence(fibonacci, max);
-  response.classList.add("container", "bg-dark", "bg-gradient", "text-white", "p-4", "mt-1");
-  fibonacci.forEach((printSequence) =>{
-    (printSequence===fibonacci[fibonacci.length-1]) ?
-      response.textContent+=(`${printSequence}`) :
-      response.textContent+=(`${printSequence}, `) 
-  });
-})
+sequencePrint();
+function sequencePrint(){
+  sendNumber.addEventListener("click", ()=>{
+    max = parseInt(num.value);
+    if(max>999 || isNaN(max)){
+      num.value="";
+      num.placeholder="3 Cifre, no caratteri";
+      response.textContent="";
+      response.classList.remove("container", "bg-dark", "bg-gradient", "text-white", "p-4", "mt-1");
+      return false;
+    }
+    let fibonacci=[0,1];
+    response.textContent="";
+    // console.log(fibonacci[0]);//DEBUG
+    // console.log(fibonacci[1]);//DEBUG
+    fibonacciSequence(fibonacci, max);
+    response.classList.add("container", "bg-dark", "bg-gradient", "text-white", "p-4", "mt-1");
+    fibonacci.forEach((printSequence) =>{
+      (printSequence===fibonacci[fibonacci.length-1]) ?
+        response.textContent+=(`${printSequence}`) :
+        response.textContent+=(`${printSequence}, `) 
+    });
+  })
+}
 
 function fibonacciSequence(e, max){
   for(i=e.length; i<=max;i++){
